8e741642407725a89caf21599d3e3c4ae34453a1
zabwie <bughasen08@gmail.com>
2025-07-16 23:39:52 -0400

    fix: allow images from Firebase Storage domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
 };
 
 export default nextConfig;
